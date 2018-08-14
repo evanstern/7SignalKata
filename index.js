18dd02a3acd51905478b54d3ad8f7f79f488ac4e
Evan Stern <evanmicahstern@gmail.com>
2018-08-14 10:54:40 -0400

    Test for adding 1 and 2 numbers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const add = (a) => {
-  const aNum = Number(a);
-  return aNum;
+  const numberStrings = a.split(',');
+  return numberStrings.map(x => Number(x)).reduce((a, b) => a + b, 0);
 };
 
 const app = { add };
