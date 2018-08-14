569c7548f23f07784a4c0e93e9ef467c8da57d94
Evan Stern <evanmicahstern@gmail.com>
2018-08-14 11:02:03 -0400

    Test for arbitrary number of arguments

--- test/index.js
+++ test/index.js
@@ -4,6 +4,15 @@ const chai = require('chai');
 const should = chai.should();
 
 describe('add', function () {
+  const createArguments = (max) => {
+    const args = [];
+    const numArgs = Math.floor(Math.random() * max) + 1;
+    for (i = 0; i < numArgs; i++) {
+      args.push(Math.floor(Math.random() * 10));
+    }
+    return args;
+  };
+
   it('should add an empty string', function () {
     Kata.add('').should.equal(0);
   });
@@ -14,5 +23,12 @@ describe('add', function () {
 
   it('should add two numbers', function () {
     Kata.add('1,2').should.equal(3);
+  });
+
+  it('should add an arbitrary number of arguments', function () {
+    const numArgs = 2 + Math.ceil(Math.random() * 10);
+    const args = createArguments(numArgs);
+    const expected = args.reduce((a, b) => a + b, 0);
+    Kata.add(args.join(',')).should.equal(expected);
   });
 });
