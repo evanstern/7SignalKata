9cf76158363c368c4fd210054c6ff62f13e5bc47
Evan Stern <evanmicahstern@gmail.com>
2018-08-14 12:15:41 -0400

    Refactor out delimiter logic

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const findNegatives = (numbers) => numbers.filter(n => n < 0);
 const trimMax = (numbers) => numbers.filter(n => n <= 1000);
 const formatDelimiter = (d) => d.replace(/^\[/, '').replace(RegExp(']$'), '');
 
+const getDelimiterString = (rawDelimiterString) => {
+  const delimiters = rawDelimiterString.split('][').map(
+    d => formatDelimiter(d)
+  ).filter(x => x.length);
+  return delimiters.map(x => `(${x})`);
+};
+
 const add = (str) => {
   const match = str.match(/\/\/.+\n/);
 
   const args = match ? str.replace(match[0], '') : str;
-  const rawDelimiters = match ? match[0].replace('//', '').replace('\n', '') : ',';
-  const delimiters = rawDelimiters.split('][').map(d => formatDelimiter(d)).filter(x => x.length);
-  const delimiterString = delimiters.map(x => `(${x})`);
+  const delimiterString = getDelimiterString(
+    match ? match[0].replace('//', '').replace('\n', '') : ','
+  );
 
   const regex = RegExp(`[${delimiterString}\n]+`);
   const numberStrings = args.split(regex);
-
   const numbers = numberStrings.map(x => Number(x));
 
   const negativeNumbers = findNegatives(numbers);
